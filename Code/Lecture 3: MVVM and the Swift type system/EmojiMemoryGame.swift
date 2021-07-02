df943134829e8a85e06d07629eae3ebbb2811070
Aozh1 <hfaz@outlook.com>
2021-07-02 16:28:56 +0800

    upload lecture3 code

---
+++ Code/Lecture 3: MVVM and the Swift type system/EmojiMemoryGame.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+
+
+
+class EmojiMemoryGame {
+
+   static let emojis = ["🚗", "🚁", "🚀", "🐶","🐕‍🦺", "🐩", "🐺", "🦊", "🦝", "🐈", "🐯", "🐴", "🦓", "🐃", "🦔", "🐻‍❄️", "🐨", "🐼","🍮", "🍥","🍷", "🏒","🎿", "🪀"]
+   static func createMemoryGame() -> MemoryGame<String> {
+        
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+               emojis[pairIndex]
+               
+           }
+    }
+
+    private var model: MemoryGame<String> = createMemoryGame()
+   
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+
+}
---
+++ Code/Lecture 3: MVVM and the Swift type system/MemoryGame.swift
@@ -0,0 +1,31 @@
+
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // add numberofpairsofcards *2
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+
+
+        }
+        
+    }
+ 
+    struct Card {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+    
+}
